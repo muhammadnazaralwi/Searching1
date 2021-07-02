daecc48a1363f6162e6d4d2694a888b084fb1b05
Muhammad Nazar Alwi <20104038@ittelkom-pwt.ac.id>
2021-07-02 10:04:26 +0700

    Add `bubbleSort()` method with O(N²) complexity

--- src/main/java/com/nazaralwi/Main.java
+++ src/main/java/com/nazaralwi/Main.java
@@ -4,4 +4,18 @@ public class Main {
     public static void main(String[] args) {
 
     }
+
+    private static int[] bubbleSort(int[] data) {
+        int temp;
+        for (int i = 1; i < data.length; i++) {
+            for (int j = data.length - 1; j >= i ; j--) {
+                if (data[j] < data[j-1]) {
+                    temp = data[j];
+                    data[j] = data[j-1];
+                    data[j-1] = temp;
+                }
+            }
+        }
+        return data;
+    }
 }
